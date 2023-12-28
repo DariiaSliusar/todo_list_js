f19542d4373141ef9a48c40b258385a6f9ba2093
Dariia Sliusar <dariiasliusar@gmail.com>
2023-12-28 20:08:18 +0100

    next commit to do list

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const addBtn = document.querySelector("#add - btn");
+const addBtn = document.querySelector("#add-btn");
 const newTaskInput = document.querySelector("#wrapper input");
 const tasksContainer = document.querySelector("#tasks");
 const error = document.getElementById("error");
@@ -10,22 +10,36 @@ const displayCount = (taskCount) => {
 };
 
 const addTask = () => {
-    const taskName = newTaskInput.ariaValueMax.trim();
+    const taskName = newTaskInput.value.trim();
     error.style.display = "none";
-    if (!taskname) {
+    if (!taskName) {
         setTimeout(() => {
             error.style.display = "block";
         }, 200);
         return;
     }
-}
-const task = `<div class="task">
+    
+    const task = `<div class="task">
 <input type="checkbox" class="task-check">
 <span class="taskname">${taskName}</span>
 <button class="edit">
-
+<i class="fa-solid fa-pen-to-square"></i>
 </button>
-<button class="delete"
-
+<button class="delete">
+<i class="fa-solid fa-trash"></i>
 </button>
 </div>`;
+    
+    tasksContainer.insertAdjacentHTML("beforeend", task);
+
+    const deleteButtons = document.querySelectorAll(".delete");
+    deleteButtons.forEach((button) => {
+        button.onclick = () => {
+            button.parentNode.remove();
+            taskCount = -1;
+            displayCount(taskCount);
+        };
+    });
+
+};
+addBtn.addEventListener("click", addTask);
